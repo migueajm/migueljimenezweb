0128c59450fb75f20835f0278ae369aba653c736
Miguel Angel Jimenez <miguejm@outlook.es>
2022-03-20 13:56:51 -0600

    test

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,58 +1,62 @@
-const btnMenu =  document.querySelector('#btnMenu');
-const overlay = document.querySelector('.overlay');
-const body = document.querySelector('body');
-const header = document.querySelector('.header');
-const fadeElems = document.querySelectorAll('.has-fade');
+const btnMenu = document.querySelector("#btnMenu");
+const overlay = document.querySelector(".overlay");
+const body = document.querySelector("body");
+const header = document.querySelector(".header");
+const fadeElems = document.querySelectorAll(".has-fade");
+const dateDev = document.querySelector("#date-dev");
+let date = new Date();
+let year = date.getFullYear();
 
+dateDev.append(`2021-${year}`);
 function toggleMenu() {
-  if(header.classList.contains('open')){ // Close Hamburger Menu
-    body.classList.remove('noscroll');
-    header.classList.remove('open');    
+  if (header.classList.contains("open")) {
+    // Close Hamburger Menu
+    body.classList.remove("noscroll");
+    header.classList.remove("open");
     fadeElems.forEach((element) => {
-      element.classList.remove('fade-in');
-      element.classList.add('fade-out');
+      element.classList.remove("fade-in");
+      element.classList.add("fade-out");
+    });
+  } else {
+    // Open Hamburger Menu
+    body.classList.add("noscroll");
+    header.classList.add("open");
+    fadeElems.forEach((element) => {
+      element.classList.remove("fade-out");
+      element.classList.add("fade-in");
     });
   }
-  else { // Open Hamburger Menu
-    body.classList.add('noscroll');
-    header.classList.add('open');
-    fadeElems.forEach((element) => {
-      element.classList.remove('fade-out');
-      element.classList.add('fade-in');
-    });
-  }  
 }
 
-btnMenu.addEventListener('click', () => {
-  console.log('click menu');
+btnMenu.addEventListener("click", () => {
+  console.log("click menu");
   toggleMenu();
 });
 
-const menuItems = document.querySelectorAll('.header__menu a');
+const menuItems = document.querySelectorAll(".header__menu a");
 
 menuItems.forEach((item) => {
-  item.addEventListener('click', () => {
+  item.addEventListener("click", () => {
     toggleMenu();
   });
-}); 
-const links = document.querySelectorAll('.header__links li a');
+});
+const links = document.querySelectorAll(".header__links li a");
 links.forEach((link) => {
-  link.addEventListener('click', (action) => {
+  link.addEventListener("click", (action) => {
     /* console.log(action.target); */
-    links.forEach((link) => link.classList.remove('active'));
-    action.target.classList.add('active');
+    links.forEach((link) => link.classList.remove("active"));
+    action.target.classList.add("active");
   });
 });
 
-const tooggleSwitch = document.querySelector('#toggleSwitch');
-let theme = document.querySelector('[data-theme]');
-tooggleSwitch.addEventListener('click', () => {
-  if(theme.getAttribute('data-theme') == 'light') {
-    theme.setAttribute('data-theme', 'dark');
-    tooggleSwitch.classList.toggle('darkmode');
-  }
-  else {
-    theme.setAttribute('data-theme', 'light');
-    tooggleSwitch.classList.toggle('darkmode');
+const tooggleSwitch = document.querySelector("#toggleSwitch");
+let theme = document.querySelector("[data-theme]");
+tooggleSwitch.addEventListener("click", () => {
+  if (theme.getAttribute("data-theme") == "light") {
+    theme.setAttribute("data-theme", "dark");
+    tooggleSwitch.classList.toggle("darkmode");
+  } else {
+    theme.setAttribute("data-theme", "light");
+    tooggleSwitch.classList.toggle("darkmode");
   }
 });
--- resources/js/projects.js
+++ resources/js/projects.js
@@ -1,27 +1,135 @@
-var projects = [
-  {
-    'name': 'Solicitudes de servico de equipo de bienes informaticos',
-    'src': 'resources/img/projects/appcrode.jpg',
-    'description': 'lorem insup lorem indup lorem insup',
-    'date': 'March-July 2021',
-    'alt': 'Web Application Crode'
-  },
-  {
-    'name': 'migueljimenezweb',
-    'src': 'resources/img/projects/majm97.jpg',
-    'description': 'Portfolio web',
-    'date': 'July-Agust 2021',
-    'alt': 'migueljimenezweb'
-  }
-]
+let src = "resources/img/code/";
+let author = document.querySelector("#author");
+let authorFooter = document.querySelector("#authorFooter");
+let authorDescription = document.querySelector("#authorDescription");
+let dauthorDev = document.querySelector("#authorDev");
+let effect = document.querySelector("#effect1");
+let skills = document.querySelector("#skills");
 
-var item = projects.map((project) => {
-  return "<div class='card'>" + 
-          "<img src='" + project.src + "' alt='" + project.alt + "'>" +
-          "<div class='card__container'>" +
-            "<h4>" + project.name + "</h4>" +
-            "<a class='button' href='#'>See</a>" +
-         "</div></div>";
-});
-document.querySelector('.portfolio__projects').innerHTML = item;
-console.log(projects);
+let menu = {
+  navbar: ["Home", "Portfolio", "Services", "Resume"],
+  service: [
+    "Web design",
+    "Web application",
+    "Mobile application Android",
+    "Mobile application IOS",
+  ],
+};
+
+let projects = {
+  migue: {
+    name: "Miguel Angel Jimenez",
+    description: "Information and Communications Technologies Engineer",
+    dev: "Web Developer",
+    title: "Miguel Angel Jimenez | Portfolio",
+    skill: {
+      code: {
+        html: {
+          name: "HTML",
+          image: src + "html.png",
+        },
+        css: {
+          name: "CSS",
+          image: src + "css.png",
+          framework: {
+            sass: {
+              name: "SASS",
+              image: src + "sass.png",
+            },
+            materialize: {
+              name: "Materialize",
+              image: src + "materialize.png",
+            },
+            bootstrap: {
+              name: "Bootstrap",
+              image: src + "bootstrap.png",
+            },
+            tailwind: {
+              name: "Tailwind-CSS",
+              image: src + "tailwind.png",
+            },
+          },
+        },
+        JavaScript: {
+          name: "JavaScript",
+          image: src + "js.png",
+          framework: {
+            react: {
+              name: "React",
+              image: src + "react.png",
+            },
+          },
+        },
+        php: {
+          name: "PHP",
+          image: src + "php.png",
+          framework: {
+            laravel: {
+              name: "Laravel",
+              image: src + "laravel.png",
+            },
+            symfony: {
+              name: "Symfony",
+              image: src + "symfony.png",
+            },
+          },
+        },
+        sql: {
+          name: "SQL",
+          image: src + "sql.png",
+        },
+      },
+      tool: {
+        terminal: {
+          name: "Terminal",
+          image: src + "terminal.png",
+        },
+        git: {
+          name: "GIT",
+          image: src + "git.png",
+        },
+        mysql: {
+          name: "GIT",
+          image: src + "mysql.png",
+        },
+        potsgresql: {
+          name: "GIT",
+          image: src + "postgresql.png",
+        },
+        composer: {
+          name: "GIT",
+          image: src + "composer.png",
+        },
+      },
+    },
+  },
+  crode: {
+    name: "Solicitudes de servico de equipo de bienes informaticos",
+    src: "resources/img/projects/appcrode.jpg",
+    description:
+      "<p>Computer asset equipment service requests<br>Web application for the administration & control of the entire IT department team</p>",
+    date: "March-July 2021",
+    url: "Web Application Crode",
+  },
+  majm: {
+    name: "migueljimenezweb",
+    src: "resources/img/projects/majm97.jpg",
+    description: "Portfolio web",
+    date: "July-Agust 2021",
+    url: "https://migueajm.github.io/migueljimenezweb/",
+  },
+  kurigage: {
+    name: "Listado de precios",
+    src: "resources/img/projects/kurigage.jog",
+    description:
+      "Listado de precios para facilitar a los usuarios la busqueda de articulos.",
+    date: "January-March 2022",
+    urlt: "http://miportal.appskurigage.com/login:",
+  },
+};
+author.append(`${projects.migue.name}`);
+authorFooter.append(`${projects.migue.name}`);
+authorDescription.append(`${projects.migue.description}`);
+authorDev.append(`${projects.migue.dev}`);
+effect.innerHTML =
+  "<div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>";
